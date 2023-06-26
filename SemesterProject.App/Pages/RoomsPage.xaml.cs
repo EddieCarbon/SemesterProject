40e3a48462fb2892235f1e3a1abc7adfd192f702
jakub.tomiczek <jakub.tomiczek@DESKTOP-OU0FTRT>
2023-06-26 16:08:30 +0200

    Add schema logic for RoomsPage

--- SemesterProject.App/Pages/RoomsPage.xaml.cs
+++ SemesterProject.App/Pages/RoomsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SemesterProject.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,96 @@ namespace SemesterProject.App.Pages
     /// </summary>
     public partial class RoomsPage : Page
     {
+        public List<User> DatabaseUsers { get; private set; }
+
         public RoomsPage()
         {
             InitializeComponent();
+        }
+
+        public void Create()
+        {
+            using (DataContext context = new DataContext())
+            {
+                var name = NameTextBox.Text;
+                var address = AddressTextBox.Text;
+
+                if (name != null && address != null)
+                {
+                    context.Users.Add(new User() { Name = name, Address = address });
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void Read()
+        {
+            using (DataContext context = new DataContext())
+            {
+                DatabaseUsers = context.Users.ToList();
+                ItemList.ItemsSource = DatabaseUsers;
+            }
+        }
+
+        public void Update()
+        {
+            using (DataContext context = new DataContext())
+            {
+                User selectedUser = ItemList.SelectedItem as User;
+
+                var name = NameTextBox.Text;
+                var address = AddressTextBox.Text;
+
+                if (name != null && address != null)
+                {
+                    User user = context.Users.Find(selectedUser.Id);
+                    user.Name = name;
+                    user.Address = address;
+
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void Delete()
+        {
+            using (DataContext context = new DataContext())
+            {
+                User selectedUser = ItemList.SelectedItem as User;
+
+                if (selectedUser != null)
+                {
+                    User user = context.Users.Single(x => x.Id == selectedUser.Id);
+
+                    context.Remove(user);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Create();
+        }
+
+        private void ReadButton_Click(object sender, RoutedEventArgs e)
+        {
+            Read();
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Update();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            Delete();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            ItemList.Items.Clear();
         }
     }
 }
